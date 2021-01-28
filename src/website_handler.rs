aa05be7b64e70100762383b61f79a7246d24893a
nick@deploy.at <mikowhy@tutanota.com>
2021-01-28 23:11:30 +0100

    feat: routing

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,4 +1,4 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
@@ -6,5 +6,13 @@ pub struct WebsiteHandler;
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
     Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string()));
+    match request.method() {
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+        "/hi" => Response::new(StatusCode::Ok, Some("<h1>Hi</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      },
+      _ => Response::new(StatusCode::NotFound, None),
+    }
   }
 }
