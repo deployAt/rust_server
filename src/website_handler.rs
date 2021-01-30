808406152efb44eb055ddcfd7550d591bccb775f
nick@deploy.at <mikowhy@tutanota.com>
2021-01-30 11:16:26 +0100

    feat: serving files

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,13 +1,32 @@
 use super::http::{Method, Request, Response, StatusCode};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
-  public_path: String
+  public_path: String,
 }
 
 impl WebsiteHandler {
   pub fn new(public_path: String) -> Self {
     Self { public_path }
+  }
+
+  fn read_file(&self, file_path: &str) -> Option<String> {
+    let path = format!("{}/{}", self.public_path, file_path);
+
+    match fs::canonicalize(path) {
+      Ok(path) => {
+        if path.starts_with(&self.public_path) {
+          fs::read_to_string(path).ok()
+        } else {
+          println!("Error traversal attack: {}", file_path);
+          None
+        }
+      }
+      Err(_) => None,
+    }
+
+    fs::read_to_string(path).ok()
   }
 }
 
@@ -16,9 +35,12 @@ impl Handler for WebsiteHandler {
     Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string()));
     match request.method() {
       Method::GET => match request.path() {
-        "/" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+        "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
         "/hi" => Response::new(StatusCode::Ok, Some("<h1>Hi</h1>".to_string())),
-        _ => Response::new(StatusCode::NotFound, None),
+        path => match self.read_file(path) {
+          Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+          None => Response::new(StatusCode::NotFound, None),
+        },
       },
       _ => Response::new(StatusCode::NotFound, None),
     }
